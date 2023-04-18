8096e87bb9be9e74a663419484121d51aa7ea589
Stephen <sfritsen@gmail.com>
2023-04-17 22:00:34 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const sayHelloManyTimes = (times) => new Array(times).fill(1).map((_, i) => `Hello ${i + 1}`);
+
+const helloDiv = document.createElement('div');
+helloDiv.innerHTML = sayHelloManyTimes(10).join('<br/>');
+document.body.append(helloDiv);

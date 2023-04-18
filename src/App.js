bdd2a5baf5f993a5a6888f5f682fe185db809408
Stephen <sfritsen@gmail.com>
2023-04-17 22:24:33 -0600

    Readme added and App.js

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+	return <div>Hello from the app!</div>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-const sayHelloManyTimes = (times) => new Array(times).fill(1).map((_, i) => `Hello ${i + 1}`);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const helloDiv = document.createElement('div');
-helloDiv.innerHTML = sayHelloManyTimes(10).join('<br/>');
-document.body.append(helloDiv);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
